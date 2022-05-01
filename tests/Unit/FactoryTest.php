eeb43baef9e5656dcd659cf83aa27851c726c1e8
Ron Appleton <ron.appleton@gmail.com>
2022-05-02 00:27:36 +0100

    Add Factory Tests

--- tests/Unit/FactoryTest.php
+++ tests/Unit/FactoryTest.php
@@ -7,14 +7,78 @@ namespace RonAppleton\GeoJson\Tests\Unit;
 use PHPUnit\Framework\TestCase;
 use RonAppleton\GeoJson\Enums\GeoJsonType;
 use RonAppleton\GeoJson\Objects\Factory as ObjectFactory;
+use RonAppleton\GeoJson\Objects\Feature;
+use RonAppleton\GeoJson\Objects\FeatureCollection;
+use RonAppleton\GeoJson\Objects\GeometryCollection;
+use RonAppleton\GeoJson\Objects\LineString;
+use RonAppleton\GeoJson\Objects\MultiLineString;
+use RonAppleton\GeoJson\Objects\MultiPoint;
+use RonAppleton\GeoJson\Objects\MultiPolygon;
 use RonAppleton\GeoJson\Objects\Point;
+use RonAppleton\GeoJson\Objects\Polygon;
 
 class FactoryTest extends TestCase
 {
+    public function testMakeFeature(): void
+    {
+        $feature = ObjectFactory::make(GeoJsonType::Feature);
+        
+        $this->assertInstanceOf(Feature::class, $feature);
+    }    
+    
+    public function testMakeFeatureCollection(): void
+    {
+        $featureCollection = ObjectFactory::make(GeoJsonType::FeatureCollection);
+
+        $this->assertInstanceOf(FeatureCollection::class, $featureCollection);
+    }
+    
+    public function testMakeGeometryCollection(): void
+    {
+        $geometryCollection = ObjectFactory::make(GeoJsonType::GeometryCollection);
+        
+        $this->assertInstanceOf(GeometryCollection::class, $geometryCollection);
+    }    
+    
+    public function testMakeLineString(): void
+    {
+        $linestring = ObjectFactory::make(GeoJsonType::LineString);
+        
+        $this->assertInstanceOf(LineString::class, $linestring);
+    }    
+    
+    public function testMakeMultiLineString(): void
+    {
+        $multiLineString = ObjectFactory::make(GeoJsonType::MultiLineString);
+        
+        $this->assertInstanceOf(MultiLineString::class, $multiLineString);
+    }    
+    
+    public function testMakeMultiPoint(): void
+    {
+        $multiPoint = ObjectFactory::make(GeoJsonType::MultiPoint);
+        
+        $this->assertInstanceOf(MultiPoint::class, $multiPoint);
+    }    
+    
+    public function testMakeMultiPolygon(): void
+    {
+        $multiPolygon = ObjectFactory::make(GeoJsonType::MultiPolygon);
+        
+        $this->assertInstanceOf(MultiPolygon::class, $multiPolygon);
+    }   
+    
     public function testMakePoint(): void
     {
         $point = ObjectFactory::make(GeoJsonType::Point);
         
         $this->assertInstanceOf(Point::class, $point);
+    }   
+    
+    public function testMakePolygon(): void
+    {
+        $polygon = ObjectFactory::make(GeoJsonType::Polygon);
+        
+        $this->assertInstanceOf(Polygon::class, $polygon);
     }
 }
